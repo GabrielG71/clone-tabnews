b746170878f8f5985e77d3207149108e41663298
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2025-07-03 00:42:28 +0000

    Alterando o texto do index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <p>Oi Mãe, tudo bem? Quero que por meio desse site você entenda o quanto você é importante para mim. Pois, quando o curso requisitou para fazer um site para alguém especial eu escolhi você.</p>;
+    return <p>Oi Mãe, tudo bem? Quero que por meio desse site você entenda o quanto você é importante para mim. Pois, quando o curso requisitou para fazer um site para alguém especial eu escolhi você. ❤️</p>;
 }
 
 export default Home;

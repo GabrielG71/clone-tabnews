104d0cfd5db16ab8c2791aa105cfb9690fd76a9e
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2025-07-03 21:29:02 +0000

    Adicionando os comandos  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br feito no https://curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Oi Mãe, tudo bem? Quero que por meio desse site você entenda o quanto você é importante para mim. Pois, quando o curso requisitou para fazer um site para alguém especial eu escolhi você. ❤️</h1>;
+function Home() {
+  return (
+    <h1>
+      Oi Mãe, tudo bem? Quero que por meio desse site você entenda o quanto você
+      é importante para mim. Pois, quando o curso requisitou para fazer um site
+      para alguém especial eu escolhi você. ❤️
+    </h1>
+  );
 }
 
 export default Home;

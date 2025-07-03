5e6dd130be4a588638e414d98d0b2402df5c28cc
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2025-07-03 00:43:56 +0000

    Alterando o texto novamente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <p>Oi Mãe, tudo bem? Quero que por meio desse site você entenda o quanto você é importante para mim. Pois, quando o curso requisitou para fazer um site para alguém especial eu escolhi você. ❤️</p>;
+    return <h1>Oi Mãe, tudo bem? Quero que por meio desse site você entenda o quanto você é importante para mim. Pois, quando o curso requisitou para fazer um site para alguém especial eu escolhi você. ❤️</h1>;
 }
 
 export default Home;

a34a0dd6ba9e3bcfab5a018b71f00936619e1769
GabrielG71 <gabrielgoncalves2981@gmail.com>
2025-10-23 23:57:13 -0300

    test(calculator): testing a simple calculator model

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,16 @@
-test("espera que 1 seja igual a 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora");
+
+test("somar 2 + 2 = 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 100 + 5 = 105", () => {
+  const resultado = calculadora.somar(100, 5);
+  expect(resultado).toBe(105);
+});
+
+test("somar banana + 2 = erro", () => {
+  const resultado = calculadora.somar("banana", 2);
+  expect(resultado).toBe("erro");
 });
